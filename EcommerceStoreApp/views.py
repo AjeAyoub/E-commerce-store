1771506a92393a4e33e7723c2bab01d51786fa0e
AjeAyoub <ajeddabayoub.official@gmail.com>
2024-05-21 14:17:03 -0700

    add cart loop

--- EcommerceStoreApp/views.py
+++ EcommerceStoreApp/views.py
@@ -8,7 +8,17 @@ def store(request):
     return render(request, 'pages/store.html', context)
 
 def cart(request):
-    context = {}
+
+    
+    if request.user.is_authenticated:
+            customer = request.user.customer
+            order, created = Order.objects.get_or_create(customer=customer, complete=False)
+            items = order.orderitem_set.all()
+    else:
+                  #Create empty cart for now for non-logged in user
+                  items = []
+            
+    context = {'items':items}
     return render(request, 'pages/cart.html', context)
 
 def checkout(request):
